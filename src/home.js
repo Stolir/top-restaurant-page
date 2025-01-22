f8674ecff4920d2cb3b064cb3c73b94b847ca441
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-01-22 23:01:54 +0300

    Write home page module

---
+++ src/home.js
@@ -0,0 +1,25 @@
+import imageURL from './Assets/jorge-zapata-4nXkhLCrkLo-unsplash.jpg'
+
+const content = document.querySelector("#content");
+
+export function loadHomePage(){
+    document.body.style.backgroundImage = `url(${imageURL})`
+    content.textContent = ''
+    content.className = 'home-page';
+
+    const heroText = document.createElement("div");
+    heroText.classList.add("hero-text");
+    heroText.textContent = "Time in Every Bite";
+
+    const supportText = document.createElement("div");
+    supportText.classList.add("support-text");
+    supportText.textContent = "Because the best pasta is made with time, patience, and love."
+
+    const menuButton = document.createElement("button");
+    menuButton.id = "explore"
+    menuButton.textContent = 'Discover Our Dishes'
+
+    content.appendChild(heroText);
+    content.appendChild(supportText);
+    content.appendChild(menuButton);
+}
